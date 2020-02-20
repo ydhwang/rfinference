afe55b793794b18c8e2fefe8495d570773ed0041
Youngdeok Hwang <ydhwang.ad@gmail.com>
2020-02-20 18:13:50 -0500

    Update rfinference.R
    changed how to handle the multicores

--- R/rfinference.R
+++ R/rfinference.R
@@ -230,8 +230,8 @@ forest_summary <- function(rf_fit, design, ncore = TRUE){
   progress <- function(b) if( b %% 100 == 0) {cat(sprintf("task %d is complete\n", b))}
   opts <- list(progress=progress)
   B <- length(rf_fit$mse) # number of trees in the forest
-  if (ncore) {
-    cl <- makeCluster((detectCores(logical = FALSE) - 1))
+  if (!is.null(ncore)) {
+    cl <- makeCluster(ncore)
   }else{
     cl <- 2
   }
